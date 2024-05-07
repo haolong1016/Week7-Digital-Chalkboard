bd18125a4922a0bf34f0e9933271a1f83fddd374
haolong1016 <164804986+haolong1016@users.noreply.github.com>
2024-05-06 20:10:05 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,9 @@
+var marker = L.marker([40.7580, -73.9855]).addTo(map);
+
+var circile = L.circle ([41.7889, -87.6007], {
+    color: "red",
+    fillcolor: '#30f',
+    fillOpacity: 0.3,
+    radius:300
+}).addTo(map);
+
